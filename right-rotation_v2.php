a591ad60ea165b62abfe80898496bb1d72c00cdb
guztus <gustavs.zubars@gmail.com>
2022-10-29 12:01:07 +0300

    initial commit

---
+++ right-rotation_v2.php
@@ -0,0 +1,38 @@
+<?php
+
+// Given a string L (of length N), print out every right rotation of L until it reverts to the original string
+// line 1: word
+// line 2: word rotated by 1 char to the right
+// line 3: word rotated by 2 chars to the right
+// ...
+// line N: word rotated by N - 1 chars to the right
+// line N + 1: word
+
+// Codingame
+// eCodingam
+// meCodinga
+// ...
+
+// + should add that if the transformed word is the same as starting word, program should end
+// When input is AAAA should output:
+// AAAA
+// AAAA
+
+
+$word = 'codelex';
+$N = strlen($word);
+
+$result = $word;
+
+$letters = str_split($word);
+
+for ($i = 0; $i <= $N; $i++)
+{
+    array_unshift($letters, end($letters));
+    array_pop($letters);
+    $result = implode($letters);
+    echo $result . PHP_EOL;
+    if ($result == $word && $i !== $N) {
+        break;
+    }
+}
